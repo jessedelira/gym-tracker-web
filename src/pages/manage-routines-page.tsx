5d456dedf56d3adb20eb915ec908a4bda2354474
Jesse De Lira <jessedelira1@gmail.com>
2025-11-18 20:30:08 -0600

    Replace inline SVGs with Heroicons components

--- src/pages/manage-routines-page.tsx
+++ src/pages/manage-routines-page.tsx
@@ -8,6 +8,8 @@ import type { Routine } from '../types/routine';
 import { useRemoveActiveRoutine } from '../hooks/active-routine/use-remove-active-routine';
 import { useSetActiveRoutine } from '../hooks/active-routine/use-set-active-routine';
 import { useFetchRoutines } from '../hooks/routine/use-fetch-routines';
+import { PencilIcon } from '@heroicons/react/24/outline';
+import { StarIcon } from '@heroicons/react/24/outline';
 
 const ACTIVE_STAR_CLASSES = 'h-6 w-6 text-yellow-400 fill-current';
 const INACTIVE_STAR_CLASSES =
@@ -155,42 +157,16 @@ export default function ManageRoutines() {
                     }
                   >
                     {activeRoutine && routine.id === activeRoutine.id ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        className={ACTIVE_STAR_CLASSES}
-                      >
-                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                      </svg>
+                      <StarIcon className={ACTIVE_STAR_CLASSES} />
                     ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className={INACTIVE_STAR_CLASSES}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                        />
-                      </svg>
+                      <StarIcon className={INACTIVE_STAR_CLASSES} />
                     )}
                   </button>
                   <Link
                     to={`/training/routines/edit/${routine.id}`}
                     className="rounded-lg p-2 text-gray-600 transition-all hover:bg-gray-100"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="h-5 w-5"
-                    >
-                      <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
-                    </svg>
+                    <PencilIcon className="size-5" />
                   </Link>
                 </div>
               </div>
