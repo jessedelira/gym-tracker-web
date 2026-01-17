d8bafc3f5cbc032e93cf3088203b31cbcefcd61a
Jesse De Lira <jessedelira1@gmail.com>
2026-01-17 00:44:37 -0600

    - update package (ran npm i) - update active session workout list component to be better about   applying style on when needed

--- src/components/active-session-workout-list.tsx
+++ src/components/active-session-workout-list.tsx
@@ -8,22 +8,23 @@ interface ActiveSessionWorkoutListProps {
   activeSession: ActiveSession;
   workoutsForActiveSession: Workout[];
   // eslint-disable-next-line no-unused-vars, no-undef
-  handleCheckboxChange: (event: Event & { currentTarget: HTMLInputElement }) => void;
+  handleCheckboxChange: (
+    event: Event & { currentTarget: HTMLInputElement },
+  ) => void;
   handleCompleteSessionClick: () => void;
   isEveryWorkoutComplete: boolean; // TODO: could create a memoized value
 }
 
 export function ActiveSessionWorkoutList(props: ActiveSessionWorkoutListProps) {
-  
   function onChangeHandler(event: Event) {
-    console.log(`event: ${event}`)
-    console.log('you triggered the onChangeHandler')
+    console.log(`event: ${event}`);
+    console.log('you triggered the onChangeHandler');
   }
-  
+
   return (
-    <div class="flex h-full w-[95%] flex-col items-center">
-      <div class="w-[90%] flex-1 flex-col pt-4">
-        <div class="mb-4 w-full rounded-lg bg-gray-50 p-4">
+    <div class="flex min-h-[calc(100vh-76px)] w-full flex-col items-center bg-gray-50">
+      <div class="w-full flex-1 flex-col px-4">
+        <div class="mt-4 mb-4 w-full rounded-lg border p-4">
           <h1 class="text-base font-medium text-gray-900">
             {props.activeSession?.session?.name}
           </h1>
