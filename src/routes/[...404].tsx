b4397df563f08ac0495d69a2a5c65bd824096164
Jesse De Lira <jessedelira1@gmail.com>
2026-02-26 12:58:53 -0600

    fix style

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,20 +2,7 @@ export function NotFound() {
   // TODO: do better formatting for the NotFound case
   return (
     <div class="mt-40 flex justify-center">
-      <h1 class='mb-4 text-4xl font-bold text-red-600'>404 - Page Not Found</h1>
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
+      <h1 class="mb-4 text-4xl font-bold text-red-600">404 - Page Not Found</h1>
     </div>
   );
 }
