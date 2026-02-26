3693f96a16b23f4c5789e3a5f59f7e8307c9c317
Jesse De Lira <jessedelira1@gmail.com>
2026-02-26 11:24:55 -0600

    resolve most vulns, prisma still depends on a vulnerable version of hono

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -7,9 +7,17 @@ import { login } from '../api/services/auth.service';
 
 import { useNavigate } from '@solidjs/router';
 import { useAuth } from '../contexts/auth-context';
+import { createForm } from '@modular-forms/solid';
+
+type LoginForm = {
+  username: string,
+  password: string
+}
+
 
 export function LoginPage() {
   const [showErrorMessage, setShowErrorMessage] = createSignal(false);
+  const [loginForm, { Form, Field, FieldArray }] = createForm<LoginForm>();
   const [username, setUsername] = createSignal('');
   const [password, setPassword] = createSignal('');
   const navigate = useNavigate();
@@ -44,9 +52,13 @@ export function LoginPage() {
           </p>
         </div>
 
-        {/* Form Card */}
+        {/* Form */}
+        <Form>
+          
+        </Form>
+        {/*
         <form class="space-y-6" onSubmit={handleSignInSubmit}>
-          {/* Error Message */}
+        // Error Message HERE
           <Show when={showErrorMessage()}>
             <div class="flex items-center justify-between rounded-xl bg-red-50 px-4 py-3 text-red-700">
               <p class="text-sm font-medium">Incorrect username or password</p>
@@ -60,40 +72,41 @@ export function LoginPage() {
             </div>
           </Show>
           {/* Input Fields */}
-          <div class="space-y-4 rounded-2xl bg-white p-8 shadow-sm">
-            <input
-              type="text"
-              placeholder="Username"
-              id="username"
-              onInput={(e) => setUsername(e.currentTarget.value)}
-              class="w-full rounded-xl border-2 border-gray-200 bg-white px-4 py-3 text-gray-900 placeholder-gray-500 transition-all focus:border-blue-500 focus:ring-2 focus:ring-blue-200 focus:outline-none"
-              required
-            />
-            <PeekPasswordInput
-              id="password"
-              placeholder="Password"
-              setPassword={setPassword}
-              isRequired
-            />
-          </div>
+        <div class="space-y-4 rounded-2xl bg-white p-8 shadow-sm">
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            onInput={(e) => setUsername(e.currentTarget.value)}
+            class="w-full rounded-xl border-2 border-gray-200 bg-white px-4 py-3 text-gray-900 placeholder-gray-500 transition-all focus:border-blue-500 focus:ring-2 focus:ring-blue-200 focus:outline-none"
+            required
+          />
+          <PeekPasswordInput
+            id="password"
+            placeholder="Password"
+            setPassword={setPassword}
+            isRequired
+          />
+        </div>
 
-          {/* Action Buttons */}
-          <div class="space-y-4">
-            <button
-              class="w-full rounded-2xl bg-blue-600 px-8 py-4 text-base font-medium text-white shadow-md transition-all focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-              type="submit"
-            >
-              Sign In
-            </button>
-            <A
-              href="/"
-              class="block w-full rounded-2xl border-2 border-gray-200 bg-white px-8 py-4 text-center text-base font-medium text-gray-700 shadow-sm transition-all"
-            >
-              Back to Home
-            </A>
-          </div>
-        </form>
-      </div>
-    </main>
+        {/* Action Buttons */}
+        <div class="space-y-4">
+          <button
+            class="w-full rounded-2xl bg-blue-600 px-8 py-4 text-base font-medium text-white shadow-md transition-all focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <A
+            href="/"
+            class="block w-full rounded-2xl border-2 border-gray-200 bg-white px-8 py-4 text-center text-base font-medium text-gray-700 shadow-sm transition-all"
+          >
+            Back to Home
+          </A>
+        </div>
+      </form>
+        */}
+    </div>
+    </main >
   );
 }
