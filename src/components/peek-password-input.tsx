04b9c60094c8a54b1444a0abb3cd08429ecb19d4
Jesse De Lira <jessedelira1@gmail.com>
2026-02-26 12:07:49 -0600

    Update login form to use store for login instead of each input having a signal

--- src/components/peek-password-input.tsx
+++ src/components/peek-password-input.tsx
@@ -1,12 +1,14 @@
-import { createSignal, Setter } from 'solid-js';
+import { createSignal } from 'solid-js';
 import { Icon } from 'solid-heroicons';
 import { eyeSlash, eye } from 'solid-heroicons/outline';
+import { SetStoreFunction } from 'solid-js/store';
+import { LoginForm } from '../routes/login';
 
 interface PasswordInputProps {
   id?: string;
   placeholder?: string;
   isRequired?: boolean;
-  setPassword: Setter<string>;
+  setPassword: SetStoreFunction<LoginForm>;
 }
 
 export function PeekPasswordInput(props: PasswordInputProps) {
@@ -19,7 +21,7 @@ export function PeekPasswordInput(props: PasswordInputProps) {
         placeholder={props.placeholder}
         required={props.isRequired}
         onInput={(e) => {
-          props.setPassword(e.target.value);
+          props.setPassword("password", e.target.value);
         }}
         class="w-full rounded-xl border-2 border-gray-200 bg-white px-4 py-3 text-gray-900 placeholder-gray-500 transition-all focus:border-blue-500 focus:ring-2 focus:ring-blue-200 focus:outline-none"
         type={isShow() ? 'text' : 'password'}
